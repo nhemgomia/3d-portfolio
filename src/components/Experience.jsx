9075e52db07975232d907bc0b5db74ec85a83936
nhemgomia <gomianehemiahrenel@gmail.com>
2023-08-07 20:19:30 +0800

    feat: Show box only if typing, implement shadow

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,5 @@
 import {
+  ContactShadows,
   OrbitControls,
   Environment,
   Sky
@@ -20,12 +21,22 @@ export const Experience = () => {
       <Sky />
       <Environment preset="sunset" />
       <group position-y={-1}>
+        <ContactShadows
+          opacity={0.42}
+          scale={10}
+          blur={1}
+          far={10}
+          resolution={256}
+          color="#000000"
+        />
         <Avatar animation={animation} />
-        <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
-          <boxGeometry />
-          <meshStandardMaterial color="white" />
-        </mesh>
-        <mesh scale={5} rotation-x={-Math.PI * 0.5}>
+        {animation === "Typing" && (
+          <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
+            <boxGeometry />
+            <meshStandardMaterial color="white" />
+          </mesh>
+        )}
+        <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
           <planeGeometry />
           <meshStandardMaterial color="white" />
         </mesh>
