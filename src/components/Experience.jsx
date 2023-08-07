5677fe9acd2e948ac1a6792a7635d89984ad6e16
nhemgomia <gomianehemiahrenel@gmail.com>
2023-08-07 20:15:01 +0800

    feat: create initial scene composition

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,35 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  OrbitControls,
+  Environment,
+  Sky
+} from "@react-three/drei";
 import { Avatar } from "./Avatar";
+import { useControls } from "leva";
 
 export const Experience = () => {
+  const { animation } = useControls({
+    animation: {
+      value: "Typing",
+      options: ["Typing", "Falling", "Standing"],
+    },
+  });
+
   return (
     <>
       <OrbitControls />
+      <Sky />
+      <Environment preset="sunset" />
       <group position-y={-1}>
-        <Avatar />
+        <Avatar animation={animation} />
+        <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
+          <boxGeometry />
+          <meshStandardMaterial color="white" />
+        </mesh>
+        <mesh scale={5} rotation-x={-Math.PI * 0.5}>
+          <planeGeometry />
+          <meshStandardMaterial color="white" />
+        </mesh>
       </group>
-      <ambientLight intesity={1} />
     </>
   );
 };
